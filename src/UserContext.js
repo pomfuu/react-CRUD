100ddfb20658dc27a6e4e52f4170b9728c510c64
pomff <alincahyono@gmail.com>
2025-01-13 08:54:15 +0700

    Update UserContext.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,31 +1,14 @@
-import React, { createContext, useState, useEffect } from "react";
-import { db } from "./firebaseConfig";
-import { doc, setDoc } from "firebase/firestore";
+import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  useEffect(() => {
-    const savedUser = JSON.parse(localStorage.getItem("user"));
-    if (savedUser) {
-      setUser(savedUser);
-    }
-  }, []);
-
-  const saveUserToStorage = async (userData) => {
-    try {
-      await setDoc(doc(db, "Users", userData.id), userData);
-      localStorage.setItem("user", JSON.stringify(userData));
-      setUser(userData);
-    } catch (error) {
-      console.error("Error updating user data in Firestore:", error);
-    }
-  };
 
   return (
-    <UserContext.Provider value={{ user, setUser: saveUserToStorage }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
 };
+
